252ffd9e1d07c9040eca42e981c5a61247bd9151
Rahul Bhardwaj <rbh97995@gmail.com>
2024-04-10 10:40:41 +0530

    add register controller and bug fixes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -25,6 +25,7 @@ const uploadOnCloudinary  = async (localfilepath) => {
     }
 }
 
+export {uploadOnCloudinary}
 // cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
 //   { public_id: "olympic_flag" }, 
 //   function(error, result) {console.log(result); 
