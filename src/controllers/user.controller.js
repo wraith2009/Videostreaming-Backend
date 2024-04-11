fb1309763ab7a1891b2ebb478cf90977d084e8e6
Rahul Bhardwaj <rbh97995@gmail.com>
2024-04-11 11:45:48 +0530

    Add user account management and points

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -248,9 +248,88 @@ const refreshAceessToken = asyncHandler(async(req, res) => {
     }
 })
 
+const changeCurrentPassword = asyncHandler(async(req, res) => {
+    const {oldPassword, newPassword} = req.body
+
+    const user = await User.findById(req.user?._id)
+
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword) 
+
+    if(!isPasswordCorrect){
+        throw new ApiError(400, "Incorrect Password")
+    }
+
+    user.password = newPassword
+    await user.save({validateBeforeSave: false})
+
+    return res.status(200).json(new ApiResponse(200,{},"password Chnaged successfully"))
+})
+
+const getCurrentUser = asyncHandler(async(req, res)=> {
+    return res.status(200).json(200, req.user, "current user fetched successfully")
+})
+
+const updateAccountDetails = asyncHandler(async(req,res) => {
+    const {email} = req.body
+
+    if(!email){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const user= User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                email: email
+            }
+        },
+        {new : true}
+    ).select("-password")
+
+    return res.status(200).json(
+        new ApiResponse(200, user, "Account details updated successfully")
+    )
+})
+
+const updateUserAvatar =  asyncHandler(async(req,res) => {
+    /**
+     * req.file
+    */
+   const avatarLocalPath = req.file?.path
+
+   if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar missing")
+    }
+
+    await uploadOnCloudinary(avatarLocalPath)
+
+    if(!avatar.url){
+        throw new ApiError(400, "Error while uploading on avatar")
+    }
+
+    await User.findByIdAndUpdate(
+        req.use?._id,
+        {
+            $set: {avatar: avatar.url}
+        },
+        {
+            new:true
+        }
+    ).select("-password")
+
+    return res.status(200).json(
+        new ApiResponse(200, user , "avatar image updated successfully")
+    )
+})
+
+// will code coverImage later by own--
 export {
     registerUser,
     loginUser,
     logOutUser,
-    refreshAceessToken
+    refreshAceessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar
 }
